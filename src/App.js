3d116b883a6f76b58b7a0081604b8a71564c19b1
jennyweic <weicchan@gmail.com>
2024-03-17 09:26:39 +0000

    Test bootstrap is working creating a button

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
+        <a href="#" className="btn btn-primary">
+          Hello
+        </a>
       </header>
     </div>
   );
