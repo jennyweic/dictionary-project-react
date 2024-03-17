aaf937bcd0afc245d1ad374943df82fceca6171f
jennyweic <weicchan@gmail.com>
2024-03-17 09:56:58 +0000

    Added useState for searching keyword and returning the updated value

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function updateKeyword(event) {
+    setKeyword(event.target.value);
+  }
+
   function submit(event) {
     event.preventDefault();
-    alert("Hello");
+    alert(`Searching for ${keyword}`);
   }
 
   return (
     <div className="Dictionary">
       <form onSubmit={submit}>
-        <input type="search" autofocus={true} />
+        <input type="search" autofocus={true} onChange={updateKeyword} />
         <input type="submit" value="Search" />
       </form>
     </div>
