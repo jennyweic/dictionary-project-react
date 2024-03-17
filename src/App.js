0248e1cfae858d780b1a0eec7a2ba773168cc973
jennyweic <weicchan@gmail.com>
2024-03-17 09:37:53 +0000

    Created a Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-        <a href="#" className="btn btn-primary">
-          Hello
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <a href="#" className="btn btn-primary">
+            Hello
+          </a>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Jennifer </small>
+        </footer>
+      </div>
     </div>
   );
 }
