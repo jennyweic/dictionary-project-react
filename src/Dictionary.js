ad60fe25b33a9d9c46f18c1bd0d69f9190635018
jennyweic <weicchan@gmail.com>
2024-03-17 09:49:34 +0000

    Created standard form

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,5 +2,17 @@ import React from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  return <div className="Dictionary"> Hello from Dictionary</div>;
+  function submit(event) {
+    event.preventDefault();
+    alert("Hello");
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={submit}>
+        <input type="search" autofocus={true} />
+        <input type="submit" value="Search" />
+      </form>
+    </div>
+  );
 }
