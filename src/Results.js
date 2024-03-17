094a7fdfcb99ddafdf767b2e83540b0fe7ba3f54
jennyweic <weicchan@gmail.com>
2024-03-17 17:52:22 +0000

    Capitalise first letter of keyword

--- src/Results.js
+++ src/Results.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function Results(props) {
   if (props.results) {
     return (
-      <div className="Results">
-        <h1>keyword{props.results.antonyms}</h1>
+      <div className="Results text-capitalize">
+        <h1>{props.results.antonyms}</h1>
         <h2></h2>
         Definition: {props.results.definition}
       </div>
